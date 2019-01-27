e2707594e46c3682eb0a8b22d979db94e7c5b8d6
vagrant <vagrant@localhost.localdomain>
2019-01-27 05:52:51 +0000

    usernameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_230316) do
+ActiveRecord::Schema.define(version: 2019_01_27_040515) do
 
   create_table "histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "users_id"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 2019_01_26_230316) do
     t.integer "quantity"
     t.integer "total_amount"
     t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,8 +34,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_230316) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
